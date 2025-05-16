a7c6d12d7a8d64fe1f879ffbcdd63eadf5609e92
Damip9 <alen.birmingham@gmail.com>
2025-05-16 15:29:18 -0400

    hero coup de coeur section

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+    "./assets/**/*.css"
+  ],
+  theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        'primary': '#FAF8F3',
+        'VertSauge': '#7D9774',
+        'BrunTerre':'#5C3D2E',
+        'TexteGris':'#4F4F4F',       
+      },
+      fontFamily: {
+        dancing: ['Dancing Script', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+        playfair: ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  plugins: []
+} 
